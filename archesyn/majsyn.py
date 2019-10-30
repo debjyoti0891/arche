781b98a8b39d71fc689300f05d89578040ce21cb
debjyoti0891 <debjyoti08911@gmail.com>
2019-10-30 19:59:01 +0800

    Initial implementation of maj-decomposition

--- archesyn/majsyn.py
+++ archesyn/majsyn.py
@@ -5,8 +5,43 @@ class BitonicSort:
         self.__a = list()
         self.__asc = True;
         self.__compNetwork = list() #i ,j, direction
+        self.__debug = False 
     
+    def getNetwork(self,n):
+        self.sort([-i for i in range(n)])
+        print('Created sorting network with {} comparators'.format(\
+            len(self.__compNetwork)))
+        self.__purge()
+        print('Created majority network with {} comparators'.format(\
+            len(self.__compNetwork)))
+        return self.__compNetwork
     
+    def __purge(self):
+        N = len(self.__a)
+        if N%2 == 0 :
+            print('Majority does not exist for even number of inputs')
+            return 
+
+        majIndex = int(N/2)-1
+        touched = [False for i in range(N)]
+        touched[majIndex] = True 
+        purgeArr = []
+        i = 0
+        for comp in reversed(self.__compNetwork):
+            if touched[comp[0]] or touched[comp[1]]:
+                touched[comp[0]] = True 
+                touched[comp[1]] = True 
+            else:
+                purgeArr.append(N-1-i)
+                
+            if i % 10 == 0 and (False not in touched):
+                break 
+            i = i+1 
+        print('Purged {}/{} comparators'.format(len(purgeArr),len(self.__compNetwork)))
+        purgeArr.sort()
+        for i in reversed(purgeArr):
+            self.__compNetwork.pop(i)
+            
     def sort(self, arr):
         self.__compNetwork = []
         self.__a = arr 
@@ -31,7 +66,8 @@ class BitonicSort:
             else:
                 m = 2**lim 
             for  i in range(lo, lo+n-m):
-                print('comp {}--{} {}'.format(i,i+m,dir))
+                if self.__debug: print('comp {}--{} {}'.format(i,i+m,dir))
+                self.__compNetwork.append([i,i+m,dir])
                 self.compare(i, i+m, dir)
             self.bitonicMerge(lo, m, dir,level+1)
             self.bitonicMerge(lo+m, n-m, dir,level+1)
@@ -47,3 +83,11 @@ class BitonicSort:
 if __name__ == '__main__':
     bs = BitonicSort()
     bs.sort([1,5,2,3,4])
+    
+    
+    with open('majres.csv','w') as f:
+        for i in range(5,1002,2):
+            net = bs.getNetwork(i)
+            f.write(str(i)+','+str(len(net))+'\n')
+            
+    #print(net)
