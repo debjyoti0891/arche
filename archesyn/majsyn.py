393387f04e8abfe98bc722c7dfdac4ec5e54ea14
debjyoti_tkpad <debjyoti08911@gmail.com>
2019-10-31 16:15:05 +0800

    updated synthesis for majority

--- archesyn/majsyn.py
+++ archesyn/majsyn.py
@@ -1,5 +1,8 @@
+import datetime
 import math
+
 class BitonicSort:
+
 
     def __init__(self):
         self.__a = list()
@@ -11,34 +14,122 @@ class BitonicSort:
         self.sort([-i for i in range(n)])
         print('Created sorting network with {} comparators'.format(\
             len(self.__compNetwork)))
+        if self.__debug:print('-------')
+        for c in self.__compNetwork:
+            if self.__debug:print('{}--{} {}'.format(c[0],c[1],c[2]))
+        if self.__debug:print('---**---')
+
+
         self.__purge()
         print('Created majority network with {} comparators'.format(\
             len(self.__compNetwork)))
+
+        if self.__debug:print('-------')
+        for c in self.__compNetwork:
+            if self.__debug:print('{}--{} {}'.format(c[0],c[1],c[2]))
+        if self.__debug:print('----**---')
         return self.__compNetwork
-    
+
+    def writeNetwork(self,outfile):
+
+        N = len(self.__a)
+        compN = len(self.__compNetwork)
+        output = 'majority'
+        outf = open(outfile,'w')
+        # header
+        outf.write('//Benchmark written by "arche" on {}\n'.format(datetime.datetime.now()))
+        outf.write('// Majority-{} constructed with {} Majority-3 nodes\n'.format(N, len(self.__compNetwork)*2))
+        outf.write('module maj{} (\n'.format(N))
+        for i in range(N):
+            outf.write('v{} , '.format(i))
+        outf.write(output+' );\n')
+
+        outf.write('input ')
+        for i in range(N-1):
+            outf.write('v{} , '.format(i))
+        outf.write('v{} ;\n'.format(N-1))
+
+        outf.write('output majority;\n')
+
+        outf.write('wire ')
+        s = ''
+        for i in range(compN*2-2):
+            s = s + 'cm{} , '.format(i)
+        outf.write(s[:-2]+' ;\n\n')
+
+        # network
+        varTrack = ['v{}'.format(i) for i in range(N)]
+        c = 0
+        for comp in self.__compNetwork[:-1]:
+            print(comp)
+            in1 = comp[0]
+            in2 = comp[1]
+
+            inp1 = varTrack[in1]
+            inp2 = varTrack[in2]
+
+            out1 = 'cm{}'.format(c)
+            c = c+1
+            out2 = 'cm{}'.format(c)
+            c = c+1
+            assert(c < 2*compN-1)
+
+            if comp[2]: # ASCENDING 
+                outf.write('assign {} = {} & {};\n'.format(out1,inp1,inp2))
+                outf.write('assign {} = {} | {};\n'.format(out2,inp1,inp2))
+            else:
+                outf.write('assign {} = {} & {};\n'.format(out2,inp1,inp2))
+                outf.write('assign {} = {} | {};\n'.format(out1,inp1,inp2))
+                
+            varTrack[in1] = out1
+            varTrack[in2] = out2 
+
+        print(self.__compNetwork[-1])
+        comp = self.__compNetwork[-1]
+        inp0 = varTrack[comp[0]]
+        inp1 = varTrack[comp[1]]
+
+        majIndex = int(N/2)
+        if (comp[0] == majIndex and comp[2] == True) \
+            or (comp[1] == majIndex and comp[2] == False): #this is the input 
+            outf.write('assign majority = {} & {} ;\n'.format(inp0,inp1))
+        else:    
+            outf.write('assign majority = {} | {} ;\n'.format(inp0,inp1))
+        
+
+        # tail
+        outf.write('endmodule')
+        outf.close()
+
+
     def __purge(self):
         N = len(self.__a)
         if N%2 == 0 :
             print('Majority does not exist for even number of inputs')
             return 
 
-        majIndex = int(N/2)-1
+        majIndex = int(N/2)
         touched = [False for i in range(N)]
         touched[majIndex] = True 
         purgeArr = []
         i = 0
         for comp in reversed(self.__compNetwork):
+            if self.__debug: print('i:{} {} touched:{}'.format(i,comp,touched))
             if touched[comp[0]] or touched[comp[1]]:
                 touched[comp[0]] = True 
                 touched[comp[1]] = True 
             else:
-                purgeArr.append(N-1-i)
+                print('purging {} -> {}'.format(i, len(self.__compNetwork)-1-i))
+                purgeArr.append(len(self.__compNetwork)-1-i)
                 
             if i % 10 == 0 and (False not in touched):
                 break 
             i = i+1 
+
+
         print('Purged {}/{} comparators'.format(len(purgeArr),len(self.__compNetwork)))
         purgeArr.sort()
+        #print('purge arr:',purgeArr)
         for i in reversed(purgeArr):
             self.__compNetwork.pop(i)
             
@@ -86,8 +177,9 @@ if __name__ == '__main__':
     
     
     with open('majres.csv','w') as f:
-        for i in range(5,1002,2):
+        for i in range(5,10,4):
             net = bs.getNetwork(i)
+            bs.writeNetwork('maj{}.v'.format(i))
             f.write(str(i)+','+str(len(net))+'\n')
             
     #print(net)
